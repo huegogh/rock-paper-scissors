39f0a680700962f43d8494015e6289ab9f08e496
huegogh <huegogh42@gmail.com>
2021-01-08 11:07:16 -0800

    add button timeout to prevent multi-clicks

--- JS/script.js
+++ JS/script.js
@@ -33,7 +33,7 @@ async function getMainButtons() {
         });
     });
 
-}
+}//getMainButtons
 
 async function getChoices(numberOfPlayers) {
     console.log(numberOfPlayers);
@@ -56,9 +56,10 @@ async function getChoices(numberOfPlayers) {
             element.classList.remove('pulse')
         });
     });
-}
+}//getChoices
 
 async function getGame(rounds, twoPlayers) {
+    let buttonEnabled = true;
     numOfRounds = rounds === 0 ? 1 : rounds === 1 ? 5 : 7;
     const choices = await fetch('./HTMLObjects/choices.html').then(data => data.text());
     document.querySelector('#app').innerHTML = choices;
@@ -67,8 +68,14 @@ async function getGame(rounds, twoPlayers) {
 
     selectable.forEach((element, i) => {
         element.addEventListener('click', (e) => {
-            element.classList.remove('pulse');
-            getResults(e.target.innerText, twoPlayers);
+            console.log(buttonEnabled);
+            if(buttonEnabled){
+                buttonEnabled = false;
+                console.log('button was clicked');
+                element.classList.remove('pulse');
+                getRoundResults(e.target.innerText, twoPlayers);
+                setTimeout(() => {buttonEnabled = true}, 1000);
+            }
         });
         element.addEventListener('mouseover', () => {
             element.classList.add('pulse')
@@ -78,21 +85,21 @@ async function getGame(rounds, twoPlayers) {
             element.classList.remove('pulse')
         });
     });
-}
+}//getGame
 
 function twoPlayerText() {
     const resultsTitle = document.getElementById('resultsTitle');
 
     if (!firstChoice) {
         resultsTitle.innerText = "Player 2 avert your eyes";
-        results.innerText = 'Player 1, select a weapon';
+        results.innerText = 'Player 1 select a weapon';
     } else {
         resultsTitle.innerText = "Player 1 avert your eyes";
-        results.innerText = 'Player 2, select a weapon';
+        results.innerText = 'Player 2 select a weapon';
     }
-}
+}// twoPlayerText
 
-async function getResults(choice, twoPlayers) {
+async function getRoundResults(choice, twoPlayers) {
     if(twoPlayers) twoPlayerText();
     const results = document.getElementById('results');
     const resultsTitle = document.getElementById('resultsTitle');
@@ -130,10 +137,17 @@ async function getResults(choice, twoPlayers) {
         }
         firstChoice = false, secondChoice = false;
         roundsPlayed++;
+        if(roundsPlayed !== numOfRounds) setTimeout(() => {
+            twoPlayerText()
+        }, 1250);
     }
 
-    if (roundsPlayed === numOfRounds) {
-        document.getElementById('playArea').classList.add('zoomerOut');
+    if (roundsPlayed === numOfRounds)  getFinalResults(p2Title);
+
+}//getRoundResults
+
+function getFinalResults(p2Title) {
+    document.getElementById('playArea').classList.add('zoomerOut');
         setTimeout(() => {
             document.getElementById('playArea').classList = 'd-none';
         }, 500);
@@ -141,25 +155,17 @@ async function getResults(choice, twoPlayers) {
         document.getElementById('finalResultsText').innerText = p1Score > p2Score ? 'Player 1 Wins the Game' : p1Score < p2Score ? p2Title + ' Wins the Game' : 'Tie Game';
         const returnButton = document.getElementById('returnButton');
         returnButton.addEventListener('click', () => {
-            // element.classList.remove('pulse');
             playArea.classList.add('zoomerOut');
-            // getResults(e.target.innerText, twoPlayers);
-            console.log('return home');
             getMainButtons();
         });
-        returnButton.addEventListener('mouseover', () => {
-            returnButton.classList.add('pulse')
-        });
+        returnButton.addEventListener('mouseover', () => {returnButton.classList.add('pulse')});
 
-        returnButton.addEventListener('mouseout', () => {
-            returnButton.classList.remove('pulse')
-        });
+        returnButton.addEventListener('mouseout', () => {returnButton.classList.remove('pulse')});
         p1Score = 0;
         p2Score = 0;
         roundsPlayed = 0;
         firstChoice = false;
         secondChoice = false;
-    }
-}
+}//getFinalResults
 
 getMainButtons();
